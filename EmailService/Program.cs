1b9f2e1ccc33fa393d0841d0990a80e953e245ca
Daniil <71185355+Choffel@users.noreply.github.com>
2025-08-05 23:07:00 +0200

    complite refactoring

--- EmailService/Program.cs
+++ EmailService/Program.cs
@@ -6,24 +6,33 @@ using EmailService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Загрузка .env файла
+
 Env.Load();
 
-// Конфигурация
+
 builder.Configuration
     .AddJsonFile("appsettings.json")
     .AddJsonFile("ConfirmMail.json", optional: true)
     .AddEnvironmentVariables();
-// Регистрация сервисов
+
 builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
 builder.Services.AddScoped<IEmailTemplateProvider, EmailTemplateProvider>();
 builder.Services.AddScoped<ISmtpClientFactory, SmtpClientFactory>();
 builder.Services.AddScoped<IEmailService, EmailService.Services.EmailService>();
 
-// Контроллеры
+builder.Services.Configure<EmailSettings>(options =>
+{
+    options.Host = Environment.GetEnvironmentVariable("SMTP_HOST") ?? throw new ArgumentNullException("SMTP_HOST is not set");
+    options.Port = int.Parse(Environment.GetEnvironmentVariable("SMTP_PORT") ?? "587");
+    options.Username = Environment.GetEnvironmentVariable("SMTP_USERNAME") ?? throw new ArgumentNullException("SMTP_USERNAME is not set");
+    options.Password = Environment.GetEnvironmentVariable("SMTP_PASSWORD") ?? throw new ArgumentNullException("SMTP_PASSWORD is not set");
+    options.EnableSsl = bool.Parse(Environment.GetEnvironmentVariable("SMTP_ENABLE_SSL") ?? "true");
+    options.SenderEmail = Environment.GetEnvironmentVariable("SENDER_EMAIL") ?? throw new ArgumentNullException("SENDER_EMAIL is not set");
+});
+
 builder.Services.AddControllers();
 
-// Swagger
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
