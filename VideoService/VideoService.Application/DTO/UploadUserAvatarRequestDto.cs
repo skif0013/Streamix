420ff18aae2d20a0c794faa0f62adcb7c9cf4abe
skif <113833988+skif0013@users.noreply.github.com>
2025-08-25 17:55:50 +0300

    some fix

--- VideoService/VideoService.Application/DTO/UploadUserAvatarRequestDto.cs
+++ VideoService/VideoService.Application/DTO/UploadUserAvatarRequestDto.cs
@@ -1,12 +1,8 @@
-using Microsoft.AspNetCore.Http;
-
 namespace VideoService.Application.DTO;
 
 public class UploadUserAvatarRequestDto
 {
-    public Guid UserId { get; set; }
-
-    public string validate { get; set; }
+    public Guid UserId { get; set; } 
     public IFormFile File { get; set; }
 }
 
@@ -14,26 +10,13 @@ public class UploadUserAvatarValidator : AbstractValidator<UploadUserAvatarReque
 {
     public UploadUserAvatarValidator()
     {
-        RuleFor(x => x.UserId).NotNull().WithMessage("UserId is required");
-        
-        
-        
-        
+        RuleFor(x => x.UserId)
+            .NotNull().WithMessage("UserId is required");
         RuleFor(x => x.File)
             .NotNull().WithMessage("File is required")
             .Must(file => file.Length < 0)
             .WithMessage("File must not be empty")
-            .Must(file => file.Length <= 1 * 1024)
-            .WithMessage("File size must not exceed 5 MB");
-        
-        
-        
-        
-        
-        
-        
-        
-        RuleFor(x => x.validate)
-            .NotNull().WithMessage("validate is required__52");
+            .Must(file => file.Length <= 1 * 1024 * 1024) // 1 MB
+            .WithMessage("File size must not exceed 1 MB");
     }
 }
--- VideoService/VideoService.Application/GlobalUsing.cs
+++ VideoService/VideoService.Application/GlobalUsing.cs
@@ -1,8 +1,4 @@
-﻿//global using VideoService.Application.DTO;
-//global using VideoService.Application.Interfaces.Repositories;
-//global using VideoService.Application.Interfaces.Services;
-//global using VideoService.Application.Models;
-//global using VideoService.Core.Models;
-global using VideoService.Core.Results;
+﻿global using VideoService.Core.Results;
 global using FluentValidation;
 global using VideoService.Application.DTO;
+global using Microsoft.AspNetCore.Http;
