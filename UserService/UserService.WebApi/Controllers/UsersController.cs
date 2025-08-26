ba8e41b7f9a50591ee1434ccd538745d059ca742
skif <113833988+skif0013@users.noreply.github.com>
2025-08-26 17:40:19 +0300

    some fixes

--- UserService/UserService.WebApi/Controllers/UsersController.cs
+++ UserService/UserService.WebApi/Controllers/UsersController.cs
@@ -2,11 +2,11 @@ namespace UserService.WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UserController : ControllerBase
+    public class UsersController : ControllerBase
     {
     private readonly IUserService _userService;
     
-        public UserController(IUserService userService)
+        public UsersController(IUserService userService)
         {
             _userService = userService;
         }
